6127e2fe3cad4c914c9d0b24f14b3ef2d00b7979
NguyenVanSon <nguyenson080301@gmail.com>
2022-09-15 14:26:12 +0700

    NVS_14/09

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenVanSon_BTH.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
